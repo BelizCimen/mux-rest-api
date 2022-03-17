6174696fcb7660c0a4f6fb6cf385019415335240
BelizCimen <belizcimensevinc@gmail.com>
2022-03-17 15:26:11 +0300

    router added

--- server.go
+++ server.go
@@ -1,1 +1,21 @@
-package mux_rest_api
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func main() {
+	router := mux.NewRouter()
+	const port string = ":8000"
+	router.HandleFunc("/", func(resp http.ResponseWriter, req *http.Request) {
+		fmt.Fprintln(resp, "Up and running...")
+	})
+	router.HandleFunc("/posts", getPosts).Methods("GET")
+	router.HandleFunc("/posts", addPost).Methods("POST")
+	log.Println("Server Listening on port", port)
+	log.Fatalln(http.ListenAndServe(port, router))
+
+}
